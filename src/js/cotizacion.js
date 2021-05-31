2f2315fc685fc6f1e25ad41d2e81af1d4e2bc560
AzaelW3b <79062163+AzaelW3b@users.noreply.github.com>
2021-05-31 02:32:01 -0500

    ACT

--- src/js/cotizacion.js
+++ src/js/cotizacion.js
@@ -1,8 +1,16 @@
 const botonesMenu = document.querySelectorAll('.botones-cotizacion button');
 const contenidoCotizacion = document.querySelectorAll('.contenido-cotizacion');
 const listaServicios = document.querySelector('.listado-servicios');
-
+const serviciosIngresados = document.querySelector('.servicios-ingresados');
 let servicioActivo = null;
+const cotizacion ={
+    nombre:'',
+    telefono:'', 
+    correo:'', 
+    fecha:'',
+    hora:'', 
+    servicios:[]
+}
 document.addEventListener("DOMContentLoaded", () => {
     mostrarServicio();
     cambioSeccion();
@@ -46,7 +54,7 @@ async function mostrarServicio() {
         const { servicios } = datos;
     
         servicios.forEach(servicio => {
-            const { precio, nombre, dias } = servicio;
+            const {id, precio, nombre, dias } = servicio;
             const precioServicio = document.createElement('p');
             precioServicio.classList.add('precio');
             precioServicio.innerHTML = `<span>$</span> ${precio}`;
@@ -65,6 +73,7 @@ async function mostrarServicio() {
     
             const divServicios = document.createElement('div');
             divServicios.classList.add('servicio-cotizacion');
+            divServicios.dataset.idServicio = id;
             listaServicios.appendChild(divServicios);
             divServicios.appendChild(precioServicio);
             divServicios.appendChild(nombreServicio);
@@ -99,11 +108,14 @@ function seleccionarServicio(servicio) {
                     contadorServicios++;
                     numeroServicios.textContent = contadorServicios;
                     const servicioObj = {
+                        id: parseInt(servicioSeleccionado.dataset.idServicio),
                         precio: servicioSeleccionado.firstElementChild.textContent,
                         nombre: servicioSeleccionado.firstElementChild.nextElementSibling.textContent
                     }
                     agregarServicioTicket(servicioObj);
                 }else{
+                    const id = parseInt(servicioSeleccionado.dataset.idServicio);
+                    eliminarServicio(id);
                     contadorServicios--;
                     numeroServicios.textContent = contadorServicios;
                 }
@@ -131,7 +143,39 @@ function seleccionarServicio(servicio) {
 }
 
 function  agregarServicioTicket(servicioObj) {
-    const serviciosIngresados = document.querySelector('.servicios-ingresados');
-  
+    const {servicios} = cotizacion;
+    cotizacion.servicios = [...servicios, servicioObj];
+    
+    //limpia el html previo, ya que el contenido se repetia
+    while(serviciosIngresados.firstElementChild.nextElementSibling){
+        serviciosIngresados.removeChild(serviciosIngresados.firstElementChild.nextElementSibling);
+    }
 
+    cotizacion.servicios.forEach(servicio=>{       
+        const {nombre, precio} = servicio;
+        const pServicioIngresado = document.createElement('p');
+        pServicioIngresado.classList.add('servicio-seleccionado');
+        pServicioIngresado.innerHTML = `${nombre} <span>${precio}</span>`;
+        serviciosIngresados.appendChild(pServicioIngresado);
+    });
+   
+}
+
+function eliminarServicio(id){
+    const {servicios} = cotizacion;
+
+    cotizacion.servicios = servicios.filter(servicio => servicio.id !== id);
+    while(serviciosIngresados.firstElementChild.nextElementSibling){
+        serviciosIngresados.removeChild(serviciosIngresados.firstElementChild.nextElementSibling);
+    }
+    cotizacion.servicios.forEach(servicioRestante =>{
+        const {nombre, precio} = servicioRestante;
+        const parrafoEliminado = document.createElement('p');
+        parrafoEliminado.classList.add('servicio-seleccionado');
+        parrafoEliminado.innerHTML = `${nombre} <span>${precio}</span>`;
+        serviciosIngresados.appendChild(parrafoEliminado);
+        console.log(parrafoEliminado);
+       
+
+    });
 }
