a56d6eb658de885687fd07a60ff2c6da7daa2e2e
Azael W3b <79062163+AzaelW3b@users.noreply.github.com>
2021-07-01 22:59:51 -0500

    CONTRATO

--- build/js/servidor.js
+++ build/js/servidor.js
@@ -4,7 +4,9 @@ const path = require('path');
 const app = express();
 //metodo para entender los datos del formulario
 app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 app.use(require('./rutas'));
+//accedemos a los archivos .html
 app.use(express.static(path.join(__dirname, '../../')))
 app.listen(3000, ()=>{
     console.log('servidor iniciado...');
